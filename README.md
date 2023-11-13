caa3c7f838630b88e370cf3816f3ef89d494b196
Bennett Schwartz <business@gustycube.xyz>
2023-11-13 22:54:16 +0000

    added redirect and updated README

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Just a fun terminal simulator coded in HTML5, JS, and CSS. It combines different
 <br>
 
 `clearcache`: This command will clear any local storage. See the [local cache documnetation](./cache.md).
+<br>
+
+`github`: Redirects to the official github.
+<br>
+
+`redirect {WEBSITE}`: This command will redirect anyone to the desired website of their choice. It will correct the command if http:// or https:// are not in it.
--- main.js
+++ main.js
@@ -25,6 +25,16 @@ function changeName(newName) {
     localStorage.setItem('username', newName);
 }
 
+function redirect(redirectLink) {
+    // Check if the link starts with "http://" or "https://"
+    if (!redirectLink.startsWith("http://") && !redirectLink.startsWith("https://")) {
+        // If not, prepend "https://"
+        redirectLink = "https://" + redirectLink;
+    }
+    // Set the window location
+    window.location.href = redirectLink;
+}
+
 function handleInput(event) {
     if (event.key === "Enter") {
         const commandInput = document.getElementById("commandInput");
@@ -56,7 +66,7 @@ function executeCommand(command) {
         window.location.reload(true);
     }
     else if (command.toLowerCase().startsWith("changename")) {
-        const newName = command.substring("changename ".length).trim();
+        const newName = command.substring("changename".length).trim();
 
         if (newName === "") {
             return "'Name' not defined.";
@@ -65,7 +75,21 @@ function executeCommand(command) {
         changeName(newName);
         return `Name changed to ${name}`;
     }
-
+    else if (command.toLowerCase() === ("github")) {
+        window.location.href = "https://www.github.com/GustyCube/terminal-simulator";
+        return "Redirecting to official GitHub"
+    }
+    else if (command.toLowerCase().startsWith("redirect")) {
+        const redirectLink = command.substring("redirect".length).trim();
+        if (redirectLink === "") {
+            return "'Link' is not defined.";
+        }
+    
+        console.log("Redirecting to:", redirectLink);
+        redirect(redirectLink);
+    }
+    
+    
     else {
         return `Command not found: ${command}`;
     }
